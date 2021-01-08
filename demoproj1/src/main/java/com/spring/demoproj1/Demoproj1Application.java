b6ed720bd6580de6a66ebdb6a9eba0a40350fc0f
Shivani Juwarkar <shivanijuwarkar@gmail.com>
2021-01-08 10:58:51 +0530

    First Commit

---
+++ demoproj1/src/main/java/com/spring/demoproj1/Demoproj1Application.java
@@ -0,0 +1,13 @@
+package com.spring.demoproj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoproj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoproj1Application.class, args);
+	}
+
+}
